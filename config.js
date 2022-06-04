e40fef2b8ae19ce421220bd7cc709d1882240ada
RullGatauu <106651464+RullGatauu@users.noreply.github.com>
2022-06-04 14:30:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
+global.owner = ['6285859047172','0']
 global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
-global.sessionName = 'data'
+global.author = 'NagatoroBot'
+global.sessionName = 'session.data'
 global.prefa = ['','!','.','#']
 global.ownername = 'KurrXd'
 global.sp = '>'
